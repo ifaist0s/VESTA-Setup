55392414327ea9ff7e0911dae588992ed78945b1
ifaist0s <ifaist0s@users.noreply.github.com>
2019-11-23 10:02:19 +0200

    BugFix & Features
    Fix a bug where pool file could still exist even no site was using it.
    Added support for php7.4

--- PHP-FPM-Templates/PHP-FPM-72.sh
+++ PHP-FPM-Templates/PHP-FPM-72.sh
@@ -20,13 +20,14 @@ pm = ondemand
 pm.max_children = 16
 pm.max_requests = 4000
 pm.process_idle_timeout = 10s
+request_terminate_timeout = 40s
 pm.status_path = /status
 
 php_admin_value[upload_tmp_dir] = /home/$1/tmp
 php_admin_value[session.save_path] = /home/$1/tmp
 php_admin_value[open_basedir] = $5:/home/$1/tmp:/bin:/usr/bin:/usr/local/bin:/var/www/html:/tmp:/usr/share:/etc/phpmyadmin:/var/lib/phpmyadmin:/etc/roundcube:/var/log/roundcube:/var/lib/roundcube:/dev/urandom
 php_admin_value[upload_max_filesize] = 128M
-php_admin_value[max_execution_time] = 60
+php_admin_value[max_execution_time] = 30
 php_admin_value[post_max_size] = 128M
 php_admin_value[memory_limit] = 256M
 php_admin_value[sendmail_path] = \"/usr/sbin/sendmail -t -i -f info@$2\"
@@ -44,6 +45,7 @@ pool_file_70="/etc/php/7.0/fpm/pool.d/$2.conf"
 pool_file_71="/etc/php/7.1/fpm/pool.d/$2.conf"
 pool_file_72="/etc/php/7.2/fpm/pool.d/$2.conf"
 pool_file_73="/etc/php/7.3/fpm/pool.d/$2.conf"
+pool_file_74="/etc/php/7.4/fpm/pool.d/$2.conf"
 
 if [ -f "$pool_file_56" ]; then
     rm $pool_file_56
@@ -60,14 +62,31 @@ if [ -f "$pool_file_71" ]; then
     service php7.1-fpm restart
 fi
 
+write_file=0
 if [ ! -f "$pool_file_72" ]; then
+  write_file=1
+else
+  user_count=$(grep -c "/home/$1/" $pool_file_72)
+  if [ $user_count -eq 0 ]; then
+    write_file=1
+  fi
+fi
+if [ $write_file -eq 1 ]; then
     echo "$pool_conf" > $pool_file_72
     service php7.2-fpm restart
+fi
+if [ -f "/etc/php/7.2/fpm/pool.d/www.conf" ]; then
+    rm /etc/php/7.2/fpm/pool.d/www.conf
 fi
 
 if [ -f "$pool_file_73" ]; then
     rm $pool_file_73
     service php7.3-fpm restart
+fi
+
+if [ -f "$pool_file_74" ]; then
+    rm $pool_file_74
+    service php7.4-fpm restart
 fi
 
 exit 0
